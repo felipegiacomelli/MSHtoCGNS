5bd2fb93dfb6a0003f67f976d5ff602c176a570a
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2018-04-30 14:56:25 -0300

    Added Output.hpp to Utilities

---
+++ include/Utilities/Output.hpp
@@ -0,0 +1,27 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+
+template<typename T>
+void output(const std::vector<T>& a, std::ofstream& file) {
+	file << std::fixed << std::setprecision(3);
+	for (auto i = a.cbegin(); i != a.cend(); i ++) 
+		file << "\t" << std::setw(6) << std::right << *i;		
+	file << std::endl;
+}
+
+template<typename T>
+void output(const std::vector<std::vector<T>>& a, std::ofstream& file) {
+	file << std::fixed << std::setprecision(3) << std::right;
+	for (auto i = a.cbegin(); i != a.cend(); i ++) {
+		for (auto j = i->cbegin(); j != i->cend(); j ++) {
+			file << "\t" << std::setw(6) << std::right << *j;
+		}
+		file << std::endl;
+	}
+}
