5d5eeb57e60d25b6420d4c3a4c53c12f26779289
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2018-08-31 22:20:44 -0300

    More features added to Mender

--- Mender/source/Mender.cpp
+++ Mender/source/Mender.cpp
@@ -5,6 +5,61 @@
 #include <FileMend/SpecialCgnsCreator3D.hpp>
 #include <FileMend/WellGenerator.hpp>
 
+void printGridDataInformation(GridDataShared gridData);
+
+void createSingleRegion(GridDataShared gridData, std::string regionName) {
+	std::vector<int> elementsOnRegion;
+	for (const auto& region : gridData->regions)
+		elementsOnRegion.insert(elementsOnRegion.begin(), region.elementsOnRegion.begin(), region.elementsOnRegion.end());
+	std::stable_sort(elementsOnRegion.begin(), elementsOnRegion.end());
+
+	RegionData region;
+	region.name = regionName;
+	region.elementsOnRegion = elementsOnRegion;
+
+	gridData->regions.clear();
+	gridData->regions.emplace_back(std::move(region));
+}
+
+void applyRatio(GridDataShared gridData, const double& ratio) {
+	for (auto& coordinate: gridData->coordinates)
+		for (auto& position : coordinate)
+			position *= ratio;
+}
+
+int main() {
+	boost::property_tree::ptree iroot;
+	boost::property_tree::read_json(std::string(SCRIPT_DIRECTORY) + "ScriptMender.json", iroot);
+	std::string inputPath  = iroot.get<std::string>("path.input");
+	std::string outputPath = iroot.get<std::string>("path.output");
+
+	auto start = std::chrono::steady_clock::now();
+	SpecialCgnsReader3D reader(inputPath);
+	GridDataShared  gridData = reader.gridData;
+	auto end = std::chrono::steady_clock::now();
+	std::chrono::duration<double> elapsedSeconds = end - start;
+	std::cout << std::endl << "\tGrid path: " << inputPath;
+	std::cout << std::endl << "\tRead in  : " << elapsedSeconds.count() << " s" << std::endl;
+
+	printGridDataInformation(gridData);
+
+	WellGenerator wellGenerator(gridData, std::string(SCRIPT_DIRECTORY) + "ScriptWellGenerator.json");
+
+	createSingleRegion(gridData, "RESERVOIR");
+	applyRatio(gridData, 1.0);
+
+	start = std::chrono::steady_clock::now();
+	SpecialCgnsCreator3D creator(gridData, outputPath);
+	end = std::chrono::steady_clock::now();
+	elapsedSeconds = end - start;
+	std::cout << std::endl << "\tConverted to CGNS format in: " << elapsedSeconds.count() << " s";
+	std::cout << std::endl << "\tOutput file location       : " << creator.getFileName() << std::endl << std::endl;
+
+	printGridDataInformation(gridData);
+
+	return 0;
+}
+
 void printGridDataInformation(GridDataShared gridData) {
 	std::cout << std::endl << "\t\tnumberOfElements: " << gridData->tetrahedronConnectivity.size() + gridData->hexahedronConnectivity.size() + gridData->prismConnectivity.size() + gridData->pyramidConnectivity.size();
 	std::cout << std::endl << "\t\t\ttetrahedra: " << gridData->tetrahedronConnectivity.size();
@@ -35,34 +90,4 @@ void printGridDataInformation(GridDataShared gridData) {
 		std::cout << std::endl << "\t\t\t" << std::setw(20) << std::left << well.name     << ": " << std::setw(6) << std::right << well.linesOnWell.front()       << " - " << well.linesOnWell.back()       << " | " << well.linesOnWell.size();
 
 	std::cout << std::endl << std::endl;
-}
-
-int main() {
-	boost::property_tree::ptree iroot;
-	boost::property_tree::read_json(std::string(SCRIPT_DIRECTORY) + "ScriptMender.json", iroot);
-	std::string inputPath  = iroot.get<std::string>("path.input");
-	std::string outputPath = iroot.get<std::string>("path.output");
-
-	auto start = std::chrono::steady_clock::now();
-	SpecialCgnsReader3D reader(inputPath);
-	GridDataShared  gridData = reader.gridData;
-	auto end = std::chrono::steady_clock::now();
-	std::chrono::duration<double> elapsedSeconds = end - start;
-	std::cout << std::endl << "\tGrid path: " << inputPath;
-	std::cout << std::endl << "\tRead in  : " << elapsedSeconds.count() << " s" << std::endl;
-
-	printGridDataInformation(gridData);
-
-	WellGenerator wellGenerator(gridData, std::string(SCRIPT_DIRECTORY) + "ScriptWellGenerator.json");
-
-	start = std::chrono::steady_clock::now();
-	SpecialCgnsCreator3D creator(gridData, outputPath);
-	end = std::chrono::steady_clock::now();
-	elapsedSeconds = end - start;
-	std::cout << std::endl << "\tConverted to CGNS format in: " << elapsedSeconds.count() << " s";
-	std::cout << std::endl << "\tOutput file location       : " << creator.getFileName() << std::endl << std::endl;
-
-	printGridDataInformation(gridData);
-
-	return 0;
 }
