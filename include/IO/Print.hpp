84c2d94bf93f688725dd8dceca70ce002341687a
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2018-10-03 16:29:20 -0300

    Saving work

--- include/IO/Print.hpp
+++ include/IO/Print.hpp
@@ -3,18 +3,8 @@
 
 #include <iostream>
 #include <iomanip>
-#include <fstream>
 #include <Grid/GridData.hpp>
 
-template<class InputIt>
-void output(InputIt cbegin, InputIt cend, std::ofstream& file) {
-	for (auto i = cbegin; i != cend; i++) {
-		for (auto j = i->cbegin(); j != i->cend(); j++)
-			file << "\t" << std::setw(3) << std::right << *j;
-		file << std::endl;
-	}
-}
-
-void output(GridDataShared gridData, std::string fileName);
+void printGridDataInformation(GridDataShared gridData);
 
 #endif
